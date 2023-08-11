c01904ffa0c389d7aebf9a36d4c00f86c21aae84
atonya-bravin <bravinatonya.brani@gmail.com>
2023-08-11 21:00:15 +0300

    inclusion of the shebang and style correction

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that implements authentication
+"""
+
+
 from flask import request
+
 
 class Auth:
     """
@@ -10,14 +17,12 @@ class Auth:
         Returns: False
         """
         return False
-    
 
     def authorization_header(self, request=None) -> str:
         """
         Returns: None
         """
         return None
-    
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
