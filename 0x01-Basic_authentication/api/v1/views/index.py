556096badb4762ba8b6eca8070a28e538955d9da
atonya-bravin <bravinatonya.brani@gmail.com>
2023-08-11 12:56:21 +0300

    documentation of the entry point method

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -28,4 +28,7 @@ def stats() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
-    abort(401)
+  """
+  GET /api/v1/unauthorized
+  """
+  abort(401)
