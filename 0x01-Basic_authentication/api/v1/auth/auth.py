7459e8e87692061b57aaeeab036a37c5bc592d54
Bravin Atonya <bravinatonya.brani@gmail.com>
2023-09-28 15:42:13 +0000

    new auth file

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,48 +1,38 @@
 #!/usr/bin/env python3
-"""
-Module that implements authentication
-"""
-
-
+"""Authenticatin file"""
 from flask import request
 from typing import List, TypeVar
+User = TypeVar('User')
 
 
 class Auth:
-    """
-    This is a class that contains authorization
-    functions
-    """
+    """class managing API authentication"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        Returns: True if path not in excluded_paths
-        """
-        if path is None or excluded_paths is None:
+        """returns False - path and excluded_paths"""
+        check = path
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
-
-        if path[-1] != '/':
-            path = path + "/"
-
-        if path not in excluded_paths:
-            return True
-
-        else:
+        if path[-1] != "/":
+            check += "/"
+        if check in excluded_paths or path in excluded_paths:
             return False
+        return True
+
+        for i in excluded_paths:
+            if i[len(i) - 1] != '*':
+                return False
+            else:
+                if i[:-1] == path[:len(i) - 1]:
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """
-        validates all requests to secure the API
-        """
+        """returns None - request will be the Flask request object"""
         if request is None:
             return None
-
-        if "Authorization" not in request.keys():
-            return None
-        else:
-            return request["Authorization"]
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """
-        Returns: None
-        """
+        """returns none"""
         return None
